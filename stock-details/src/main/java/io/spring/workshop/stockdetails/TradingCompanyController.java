f1d596a2d534f50c728b7faf91bc13fbeb8ef182
bkamysz-pivotal <bkamysz@pivotal.io>
2019-01-24 09:43:51 -0800

    Change projects to Gradle

--- stock-details/src/main/java/io/spring/workshop/stockdetails/TradingCompanyController.java
+++ stock-details/src/main/java/io/spring/workshop/stockdetails/TradingCompanyController.java
@@ -1,32 +1,29 @@
 package io.spring.workshop.stockdetails;
 
-import java.time.Duration;
-
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class TradingCompanyController {
 
-	private final TradingCompanyRepository tradingCompanyRepository;
+    private final TradingCompanyRepository tradingCompanyRepository;
 
-	public TradingCompanyController(TradingCompanyRepository tradingCompanyRepository) {
-		this.tradingCompanyRepository = tradingCompanyRepository;
-	}
+    public TradingCompanyController(TradingCompanyRepository tradingCompanyRepository) {
+        this.tradingCompanyRepository = tradingCompanyRepository;
+    }
 
-	@GetMapping(path = "/details", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Flux<TradingCompany> listTradingCompanies() {
-		return this.tradingCompanyRepository.findAll();
-	}
+    @GetMapping(path = "/details", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Flux<TradingCompany> listTradingCompanies() {
+        return this.tradingCompanyRepository.findAll();
+    }
 
-	@GetMapping(path = "/details/{ticker}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Mono<TradingCompany> showTradingCompanies(@PathVariable String ticker) {
-		return this.tradingCompanyRepository.findByTicker(ticker).delayElement(Duration.ofMillis(400));
-	}
+    @GetMapping(path = "/details/{ticker}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<TradingCompany> showTradingCompanies(@PathVariable String ticker) {
+        return this.tradingCompanyRepository.findByTicker(ticker);
+    }
 
 }
